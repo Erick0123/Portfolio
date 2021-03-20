ca65a62d41c1a5e084e52d638a28b67341692cb4
Erick0123 <erickalan068@gmail.com>
2021-03-20 13:45:50 -0300

    javaScript incompleto

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //barra de progresso
-let barraProgresso = document.querySelector('.barraprogresso')
+let barraProgresso = document.querySelector('p.barraProgresso')
 //habilidade front
 let html = document.querySelector('#html')
 let css = document.querySelector('#css')
@@ -7,17 +7,32 @@ let javaScript = document.querySelector('#javascript')
 let angular = document.querySelector('#angular')
 let typeScript = document.querySelector('#typeScript')
 //habilidade back
-let spring = document.querySelector('spring')
-let mysql = document.querySelector('mysql')
-let objeto = document.querySelector('objeto')
-let logica = document.querySelector('logica')
+let spring = document.querySelector('#spring')
+let mysql = document.querySelector('#mysql')
+let objeto = document.querySelector('#objeto')
+let logica = document.querySelector('#logica')
 //habilidade soft skills
 let equipe = document.querySelector('#equipe')
 let proatividade = document.querySelector('#proatividade')
 let comunicacao = document.querySelector('#comunicacao')
 let resiliencia = document.querySelector('#resiliencia')
 
-html.addEventListener('input', function(){
+/*html.addEventListener('input', function(){
     const value = html.value;
     barraProgresso.style.setProperty('--progress', value)
+})*/
+
+    html.addEventListener('input', function(){
+        const value = html.value;
+        barraProgresso.style.setProperty('--progress',value)
+    })
+
+    css.addEventListener('input', function(){
+    const value = css.value;
+    barraProgresso.style.setProperty('--progress', value)
 })
+
+
+
+
+
